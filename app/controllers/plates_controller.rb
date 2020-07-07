668de2b208ebbe45f8546fa2e357201a9ac51a04
Yuka Itozu <yukaitozu@gmail.com>
2020-07-07 17:31:17 +0900

    updated plates controller

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -1,10 +1,33 @@
 class PlatesController < ApplicationController
-  def index
-    @restaurants = Restaurant.all
-    @plates = Plates.all
-  end
-
   def show
     @plate = Plate.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @plate = Plate.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @plate = Plate.new(plate_params)
+    @plate.restaurant = @restaurant
+    if @plate.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @plate = Plate.find(params[:id])
+    @plate.destroy
+    redirect_to restaurant_path(@plate.restaurant)
+  end
+
+  private
+
+  def plate_params
+    params.require(:plate).permit(:name, :rating)
   end
 end
