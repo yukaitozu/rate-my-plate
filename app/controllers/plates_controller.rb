d0d785880c977ebf91235a00de2e9f7641327c26
Yuka Itozu <yukaitozu@gmail.com>
2020-07-07 17:27:52 +0900

    updated plate routes

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -1,27 +1,10 @@
 class PlatesController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    @plates = Plates.all
+  end
+
   def show
     @plate = Plate.find(params[:id])
-  end
-
-  def new
-    @plate = Plate.new
-  end
-
-  def create
-    @plate = Plate.new(plate_params)
-    @plate.save
-    redirect_to plate_path(@plate)
-  end
-
-  def destroy
-    @plate = Plate.find(params[:id])
-    @plate.destroy
-    redirect_to plates_path
-  end
-
-  private
-
-  def plate_params
-    params.require(:plate).permit(:name, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    resources :plates, only: [:index, :new, :create]
+    resources :plates, only: [:new, :create]
   end
   resources :plates, only: [:show, :edit, :update, :destroy]
 end
