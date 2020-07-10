7fefea3d9f0636f4e57240f62363ea19b8b4a53e
Yuka Itozu <yukaitozu@gmail.com>
2020-07-10 20:41:08 +0900

    added validations to restaurants and plates

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,3 +1,6 @@
 class Plate < ApplicationRecord
   belongs_to :restaurant
+
+  validates :name, presence: true
+  validates :rating, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :plates, dependent: :destroy
+
+  validates :name, presence: true
+  validates :rating, presence: true
 end
