b24b2bd976feda4ca07ca80a77e70d7ba7e0e2b0
Yuka Itozu <yukaitozu@gmail.com>
2020-07-30 23:27:24 +0900

    setup cloudinary

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,5 +1,6 @@
 class Plate < ApplicationRecord
   belongs_to :restaurant
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :rating, presence: true
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :plates, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :rating, presence: true
